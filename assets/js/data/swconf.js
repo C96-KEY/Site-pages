958bfb4d178a581ee07924628896ffac15c7eca0
PROFILE GITHUB <cia@github.com>
2025-04-27 13:00:54 +0000

    Deploy website

---
+++ assets/js/data/swconf.js
@@ -0,0 +1,28 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1745587504',resources: [
+      '/assets/css/jekyll-theme-chirpy.css',
+      '/',
+      
+        '/archives/',
+      
+        '/about/',
+      
+        '/rooms/',
+      
+
+      
+      
+    ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+      ]
+    },
+
+    purge: false
+  
+};
+
